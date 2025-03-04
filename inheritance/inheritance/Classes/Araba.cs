396a6d1b0a5ac5968eedf87e5355b7e393ede8f5
aaksoyy <69424127+abdullah-aksoy@users.noreply.github.com>
2025-03-04 15:36:48 +0300

    Add files via upload

---
+++ inheritance/inheritance/Classes/Araba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance.Classes
+{
+    public class Araba
+    {
+        public void duzVites(string carName)
+        {
+            Console.WriteLine(carName+ " -> Düz viteslidir.");
+
+
+        }
+        public void otomatikVites(string carName)
+        {
+            Console.WriteLine(carName + " -> Otomatik viteslidir.");
+        }
+    }
+}
---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using inheritance.Classes;
+
+namespace inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Audi audi = new Audi();
+            audi.otomatikVites("Audi");
+
+            BMW bmw = new BMW();
+            bmw.duzVites("BMW");
+
+            Porsche porsche = new Porsche();
+            porsche.otomatikVites("Porsche");
+
+            Togg togg = new Togg();
+            togg.otomatikVites("Togg");
+
+            Toyota toyota= new Toyota();
+            toyota.duzVites("Toyota");
+
+
+        }
+    }
+}
